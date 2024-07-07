a9d07fcde7fdf338694160399a923170a1cfc395
Akhilesh Adhikari <akhileshadhikaria1@gmail.com>
2024-07-07 15:35:57 +0545

    updated package

--- 02-SERVER-STREAM/grpc-1/client/main.go
+++ 02-SERVER-STREAM/grpc-1/client/main.go
@@ -6,10 +6,11 @@ import (
 	"log"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
-	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure(), grpc.WithBlock())
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
