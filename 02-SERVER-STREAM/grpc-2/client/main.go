01001d74610d7f45c8cf2a9708786a07140a61a2
Akhilesh Adhikari <akhileshadhikaria1@gmail.com>
2024-07-07 17:11:02 +0545

    process info

---
+++ 02-SERVER-STREAM/grpc-2/client/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"grpc-2/pb"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Error connecting : %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewProcessesClient(conn)
+
+	req := &pb.ProcessRequest{}
+	stream, err := c.GetProcessesInfo(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error calling GetProcessesInfo : %v", err)
+	}
+	for {
+		resp, err := stream.Recv()
+		if err != nil {
+			break
+		}
+		log.Println(" Process Id:",resp.ProcessId)
+		log.Println(" Process Name:",resp.ProcessName)
+		log.Println(" Cpu Usage:",resp.CpuUsage)
+		log.Printf(" Memory Usage: %.2f %s",resp.MemoryUsage, resp.MemUnit)
+	}
+}
