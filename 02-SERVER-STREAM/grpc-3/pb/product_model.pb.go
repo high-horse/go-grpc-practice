6553abf0e55b155fe45f79ba14b2b219a7d781f1
Akhilesh Adhikari <akhileshadhikaria1@gmail.com>
2024-07-15 17:15:46 +0545

    server func 1

--- 02-SERVER-STREAM/grpc-3/pb/product_model.pb.go
+++ 02-SERVER-STREAM/grpc-3/pb/product_model.pb.go
@@ -25,9 +25,9 @@ type ProductResponse struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	Id          string  `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+	Id          uint64  `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
 	Title       string  `protobuf:"bytes,2,opt,name=title,proto3" json:"title,omitempty"`
-	Price       string  `protobuf:"bytes,3,opt,name=price,proto3" json:"price,omitempty"`
+	Price       float32 `protobuf:"fixed32,3,opt,name=price,proto3" json:"price,omitempty"`
 	Description string  `protobuf:"bytes,4,opt,name=description,proto3" json:"description,omitempty"`
 	Category    string  `protobuf:"bytes,5,opt,name=category,proto3" json:"category,omitempty"`
 	Image       string  `protobuf:"bytes,6,opt,name=image,proto3" json:"image,omitempty"`
@@ -66,11 +66,11 @@ func (*ProductResponse) Descriptor() ([]byte, []int) {
 	return file_product_model_proto_rawDescGZIP(), []int{0}
 }
 
-func (x *ProductResponse) GetId() string {
+func (x *ProductResponse) GetId() uint64 {
 	if x != nil {
 		return x.Id
 	}
-	return ""
+	return 0
 }
 
 func (x *ProductResponse) GetTitle() string {
@@ -80,11 +80,11 @@ func (x *ProductResponse) GetTitle() string {
 	return ""
 }
 
-func (x *ProductResponse) GetPrice() string {
+func (x *ProductResponse) GetPrice() float32 {
 	if x != nil {
 		return x.Price
 	}
-	return ""
+	return 0
 }
 
 func (x *ProductResponse) GetDescription() string {
@@ -115,6 +115,53 @@ func (x *ProductResponse) GetRating() *Rating {
 	return nil
 }
 
+type ProductList struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Products []*ProductResponse `protobuf:"bytes,1,rep,name=products,proto3" json:"products,omitempty"`
+}
+
+func (x *ProductList) Reset() {
+	*x = ProductList{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_product_model_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *ProductList) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*ProductList) ProtoMessage() {}
+
+func (x *ProductList) ProtoReflect() protoreflect.Message {
+	mi := &file_product_model_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use ProductList.ProtoReflect.Descriptor instead.
+func (*ProductList) Descriptor() ([]byte, []int) {
+	return file_product_model_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *ProductList) GetProducts() []*ProductResponse {
+	if x != nil {
+		return x.Products
+	}
+	return nil
+}
+
 type Rating struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
@@ -127,7 +174,7 @@ type Rating struct {
 func (x *Rating) Reset() {
 	*x = Rating{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_product_model_proto_msgTypes[1]
+		mi := &file_product_model_proto_msgTypes[2]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -140,7 +187,7 @@ func (x *Rating) String() string {
 func (*Rating) ProtoMessage() {}
 
 func (x *Rating) ProtoReflect() protoreflect.Message {
-	mi := &file_product_model_proto_msgTypes[1]
+	mi := &file_product_model_proto_msgTypes[2]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -153,7 +200,7 @@ func (x *Rating) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Rating.ProtoReflect.Descriptor instead.
 func (*Rating) Descriptor() ([]byte, []int) {
-	return file_product_model_proto_rawDescGZIP(), []int{1}
+	return file_product_model_proto_rawDescGZIP(), []int{2}
 }
 
 func (x *Rating) GetRate() float32 {
@@ -179,7 +226,7 @@ type ProductRequest struct {
 func (x *ProductRequest) Reset() {
 	*x = ProductRequest{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_product_model_proto_msgTypes[2]
+		mi := &file_product_model_proto_msgTypes[3]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -192,7 +239,7 @@ func (x *ProductRequest) String() string {
 func (*ProductRequest) ProtoMessage() {}
 
 func (x *ProductRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_product_model_proto_msgTypes[2]
+	mi := &file_product_model_proto_msgTypes[3]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -205,7 +252,7 @@ func (x *ProductRequest) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use ProductRequest.ProtoReflect.Descriptor instead.
 func (*ProductRequest) Descriptor() ([]byte, []int) {
-	return file_product_model_proto_rawDescGZIP(), []int{2}
+	return file_product_model_proto_rawDescGZIP(), []int{3}
 }
 
 var File_product_model_proto protoreflect.FileDescriptor
@@ -214,10 +261,10 @@ var file_product_model_proto_rawDesc = []byte{
 	0x0a, 0x13, 0x70, 0x72, 0x6f, 0x64, 0x75, 0x63, 0x74, 0x5f, 0x6d, 0x6f, 0x64, 0x65, 0x6c, 0x2e,
 	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x07, 0x70, 0x72, 0x6f, 0x64, 0x75, 0x63, 0x74, 0x22, 0xca,
 	0x01, 0x0a, 0x0f, 0x50, 0x72, 0x6f, 0x64, 0x75, 0x63, 0x74, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e,
-	0x73, 0x65, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x02,
+	0x73, 0x65, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x04, 0x52, 0x02,
 	0x69, 0x64, 0x12, 0x14, 0x0a, 0x05, 0x74, 0x69, 0x74, 0x6c, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28,
 	0x09, 0x52, 0x05, 0x74, 0x69, 0x74, 0x6c, 0x65, 0x12, 0x14, 0x0a, 0x05, 0x70, 0x72, 0x69, 0x63,
-	0x65, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x70, 0x72, 0x69, 0x63, 0x65, 0x12, 0x20,
+	0x65, 0x18, 0x03, 0x20, 0x01, 0x28, 0x02, 0x52, 0x05, 0x70, 0x72, 0x69, 0x63, 0x65, 0x12, 0x20,
 	0x0a, 0x0b, 0x64, 0x65, 0x73, 0x63, 0x72, 0x69, 0x70, 0x74, 0x69, 0x6f, 0x6e, 0x18, 0x04, 0x20,
 	0x01, 0x28, 0x09, 0x52, 0x0b, 0x64, 0x65, 0x73, 0x63, 0x72, 0x69, 0x70, 0x74, 0x69, 0x6f, 0x6e,
 	0x12, 0x1a, 0x0a, 0x08, 0x63, 0x61, 0x74, 0x65, 0x67, 0x6f, 0x72, 0x79, 0x18, 0x05, 0x20, 0x01,
@@ -225,13 +272,17 @@ var file_product_model_proto_rawDesc = []byte{
 	0x69, 0x6d, 0x61, 0x67, 0x65, 0x18, 0x06, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x69, 0x6d, 0x61,
 	0x67, 0x65, 0x12, 0x27, 0x0a, 0x06, 0x72, 0x61, 0x74, 0x69, 0x6e, 0x67, 0x18, 0x07, 0x20, 0x01,
 	0x28, 0x0b, 0x32, 0x0f, 0x2e, 0x70, 0x72, 0x6f, 0x64, 0x75, 0x63, 0x74, 0x2e, 0x52, 0x61, 0x74,
-	0x69, 0x6e, 0x67, 0x52, 0x06, 0x72, 0x61, 0x74, 0x69, 0x6e, 0x67, 0x22, 0x32, 0x0a, 0x06, 0x52,
-	0x61, 0x74, 0x69, 0x6e, 0x67, 0x12, 0x12, 0x0a, 0x04, 0x72, 0x61, 0x74, 0x65, 0x18, 0x01, 0x20,
-	0x01, 0x28, 0x02, 0x52, 0x04, 0x72, 0x61, 0x74, 0x65, 0x12, 0x14, 0x0a, 0x05, 0x63, 0x6f, 0x75,
-	0x6e, 0x74, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0d, 0x52, 0x05, 0x63, 0x6f, 0x75, 0x6e, 0x74, 0x22,
-	0x10, 0x0a, 0x0e, 0x50, 0x72, 0x6f, 0x64, 0x75, 0x63, 0x74, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73,
-	0x74, 0x42, 0x0a, 0x5a, 0x08, 0x2e, 0x2e, 0x2f, 0x70, 0x62, 0x3b, 0x70, 0x62, 0x62, 0x06, 0x70,
-	0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x69, 0x6e, 0x67, 0x52, 0x06, 0x72, 0x61, 0x74, 0x69, 0x6e, 0x67, 0x22, 0x43, 0x0a, 0x0b, 0x50,
+	0x72, 0x6f, 0x64, 0x75, 0x63, 0x74, 0x4c, 0x69, 0x73, 0x74, 0x12, 0x34, 0x0a, 0x08, 0x70, 0x72,
+	0x6f, 0x64, 0x75, 0x63, 0x74, 0x73, 0x18, 0x01, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x18, 0x2e, 0x70,
+	0x72, 0x6f, 0x64, 0x75, 0x63, 0x74, 0x2e, 0x50, 0x72, 0x6f, 0x64, 0x75, 0x63, 0x74, 0x52, 0x65,
+	0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x52, 0x08, 0x70, 0x72, 0x6f, 0x64, 0x75, 0x63, 0x74, 0x73,
+	0x22, 0x32, 0x0a, 0x06, 0x52, 0x61, 0x74, 0x69, 0x6e, 0x67, 0x12, 0x12, 0x0a, 0x04, 0x72, 0x61,
+	0x74, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x02, 0x52, 0x04, 0x72, 0x61, 0x74, 0x65, 0x12, 0x14,
+	0x0a, 0x05, 0x63, 0x6f, 0x75, 0x6e, 0x74, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0d, 0x52, 0x05, 0x63,
+	0x6f, 0x75, 0x6e, 0x74, 0x22, 0x10, 0x0a, 0x0e, 0x50, 0x72, 0x6f, 0x64, 0x75, 0x63, 0x74, 0x52,
+	0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x42, 0x0a, 0x5a, 0x08, 0x2e, 0x2e, 0x2f, 0x70, 0x62, 0x3b,
+	0x70, 0x62, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -246,19 +297,21 @@ func file_product_model_proto_rawDescGZIP() []byte {
 	return file_product_model_proto_rawDescData
 }
 
-var file_product_model_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
+var file_product_model_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
 var file_product_model_proto_goTypes = []any{
 	(*ProductResponse)(nil), // 0: product.ProductResponse
-	(*Rating)(nil),          // 1: product.Rating
-	(*ProductRequest)(nil),  // 2: product.ProductRequest
+	(*ProductList)(nil),     // 1: product.ProductList
+	(*Rating)(nil),          // 2: product.Rating
+	(*ProductRequest)(nil),  // 3: product.ProductRequest
 }
 var file_product_model_proto_depIdxs = []int32{
-	1, // 0: product.ProductResponse.rating:type_name -> product.Rating
-	1, // [1:1] is the sub-list for method output_type
-	1, // [1:1] is the sub-list for method input_type
-	1, // [1:1] is the sub-list for extension type_name
-	1, // [1:1] is the sub-list for extension extendee
-	0, // [0:1] is the sub-list for field type_name
+	2, // 0: product.ProductResponse.rating:type_name -> product.Rating
+	0, // 1: product.ProductList.products:type_name -> product.ProductResponse
+	2, // [2:2] is the sub-list for method output_type
+	2, // [2:2] is the sub-list for method input_type
+	2, // [2:2] is the sub-list for extension type_name
+	2, // [2:2] is the sub-list for extension extendee
+	0, // [0:2] is the sub-list for field type_name
 }
 
 func init() { file_product_model_proto_init() }
@@ -280,7 +333,7 @@ func file_product_model_proto_init() {
 			}
 		}
 		file_product_model_proto_msgTypes[1].Exporter = func(v any, i int) any {
-			switch v := v.(*Rating); i {
+			switch v := v.(*ProductList); i {
 			case 0:
 				return &v.state
 			case 1:
@@ -292,6 +345,18 @@ func file_product_model_proto_init() {
 			}
 		}
 		file_product_model_proto_msgTypes[2].Exporter = func(v any, i int) any {
+			switch v := v.(*Rating); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_product_model_proto_msgTypes[3].Exporter = func(v any, i int) any {
 			switch v := v.(*ProductRequest); i {
 			case 0:
 				return &v.state
@@ -310,7 +375,7 @@ func file_product_model_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_product_model_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   3,
+			NumMessages:   4,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
--- 02-SERVER-STREAM/grpc-3/server/main.go
+++ 02-SERVER-STREAM/grpc-3/server/main.go
@@ -1,13 +1,93 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	proto "grpc-3/pb"
+	"io"
+	"log"
+	"net"
+	"net/http"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 type Server struct {
 	proto.UnimplementedProductServiceServer
 }
 
-func main() {
+const URL = "https://fakestoreapi.com/products"
 
+func main() {
+	// res, err := CallHttpRequest()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(res)
+
+	// return
+
+	address := ":50051"
+	println("server starting at " + address)
+
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatalf("Couldnot start server \n", err)
+	}
+
+	serv := grpc.NewServer()
+	proto.RegisterProductServiceServer(serv, &Server{})
+
+	reflection.Register(serv)
+
+	if err := serv.Serve(listener); err != nil {
+		log.Fatalf("Could not start server \n", err)
+	}
+}
+
+func (s *Server) GetProduct(context.Context, *proto.ProductRequest) (*proto.ProductList, error) {
+	var products proto.ProductList
+	var err error
+
+	productsSlice, err := CallHttpRequest()
+	if err != nil {
+		return nil, err
+	}
+	for _, product := range productsSlice.Products {
+		products.Products = append(products.Products, product)
+	}
+	
+
+	return &products, nil
+}
+
+
+func CallHttpRequest() (*proto.ProductList, error) {
+    res, err := http.Get(URL)
+    if err != nil {
+        return nil, err
+    }
+    defer res.Body.Close()
+
+    if res.StatusCode != http.StatusOK {
+        return nil, fmt.Errorf("received non-200 response code: %d", res.StatusCode)
+    }
+
+    body, err := io.ReadAll(res.Body)
+    if err != nil {
+        return nil, err
+    }
+
+    fmt.Printf("res: %s\n", string(body)) // Print the body for debugging
+
+    var products []*proto.ProductResponse
+    err = json.Unmarshal(body, &products)
+    if err != nil {
+        return nil, err
+    }
+
+    response := &proto.ProductList{Products: products}
+    return response, nil
 }
