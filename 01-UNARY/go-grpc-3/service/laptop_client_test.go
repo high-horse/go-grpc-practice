adcae0a13b2204c8a643491ce90405b074bc0621
AkhileshAdhikari <akhileshadhikaria1@gmail.com>
2024-07-17 23:35:29 +0545

    ded

---
+++ 01-UNARY/go-grpc-3/service/laptop_client_test.go
@@ -0,0 +1,29 @@
+package service_test
+
+import (
+	"grpc-3/pb"
+	"grpc-3/service"
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+)
+
+func TestClientCreateLaptop(t *testing.T) {
+	t.Parallel()
+}
+
+func startTestLaptopServer(t *testing.T) (*service.LaptopServer, string) {
+	laptopServer := service.NewLaptopServer(service.NewInMemoryLaptopStore())
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterLaptopServiceServer(grpcServer, laptopServer)
+
+	listener, err := net.Listen("tcp", ":0")
+	require.NoError(t, err)
+
+	go grpcServer.Serve(listener)
+
+	return laptopServer, listener.Addr().String()
+}
