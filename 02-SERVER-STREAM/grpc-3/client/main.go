39db281616a00a6abfc8e94b9acd86a1b20c60fa
Akhilesh Adhikari <akhileshadhikaria1@gmail.com>
2024-07-22 13:47:45 +0545

    ss

--- 02-SERVER-STREAM/grpc-3/client/main.go
+++ 02-SERVER-STREAM/grpc-3/client/main.go
@@ -9,10 +9,29 @@ import (
 	proto "grpc-3/pb"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 const SERVER = "localhost:50051"
+
+func unaryInterseptor(
+	ctx context.Context,
+	req interface{},
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler,
+) (interface{}, error) {
+	log.Printf("intercepted Unary call: %v", info.FullMethod)
+
+	resp, err := handler(ctx, req)
+	if err != nil {
+		log.Printf("error from unary call : %v", err)
+		return nil, status.Errorf(status.Code(err), "Unary call failed, %v", err)
+	}
+
+	return resp, err
+}
+
 
 func main() {
 	log.Printf("Starting Server at %s \n", SERVER)
