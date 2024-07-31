3184c0fa516924dcbdd3dd05e62518ce5674f991
Akhilesh Adhikari <akhileshadhikaria1@gmail.com>
2024-07-31 17:30:29 +0545

    ss

--- 04-CLIENT-STREAM/grpc-1/news-client/components/menu/details/NewDetailComponent.vue
+++ 04-CLIENT-STREAM/grpc-1/news-client/components/menu/details/NewDetailComponent.vue
@@ -1,16 +1,28 @@
 <template>
+  <form @submit.prevent="handleSubmit">
     <div>
+        
         <b> Heading</b>
         <UTextarea
             color="primary"
             variant="outline"
             placeholder="Heading..."
-            model-value=""
+            v-model="formData.title"
         />
         <b>Author</b>
-        <UInput color="primary" variant="outline" placeholder="Author..." />
+        <UInput 
+          color="primary" 
+          variant="outline" 
+          placeholder="Author..." 
+          v-model="formData.author"
+        />
         <b>Source</b>
-        <UInput color="primary" variant="outline" placeholder="Source..." />
+        <UInput 
+          color="primary" 
+          variant="outline" 
+          placeholder="Source..." 
+          v-model="formData.source.name"
+        />
         <b>Description:</b>
         <UTextarea 
             ref="textareaRef" 
@@ -19,6 +31,7 @@
             placeholder="News Description..." 
             :rows="5"
             :maxrows="20"
+            v-model="formData.description"
         />
         
 
@@ -33,6 +46,7 @@
             label="Discard"
             :trailing="false"
             class="mr-2"
+            @click="discardNews"
           />
           <UButton
             icon="i-heroicons-pencil-square"
@@ -40,13 +54,43 @@
             color="primary"
             variant="solid"
             label="Save"
+            type="submit"
             :trailing="true"
+
           />
         </div>
     </div>
+
+  </form>
 </template>
 
 <script setup lang="ts">
+import type { NewsItem } from '~/types';
 
+
+const formData = ref<NewsItem >({
+  source: { id: '', name: '' },
+  title: '',
+  author: '',
+  description: '',
+  publishedAt: '',
+});
+
+// const emits = defineEmits<{
+//   (event: 'submit-newnews', submitData: NewsItem): void;
+// }>();
+
+const discardNews = () => {
+  formData.value = {
+    source: {id:'', name: ''},
+    title: "",
+    author: "",
+    description: '',
+    publishedAt: '',
+  }
+}
+const handleSubmit = () => {
+  console.log("FORMDATA", formData.value);
+}
 </script>
 
